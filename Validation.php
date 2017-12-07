2d0ef5dcb32b50749f8d5f995f8f2d2fade78c8f
Budi Pratama <boedipratama19@gmail.com>
2017-12-08 01:45:01 +0700

    Test
    sdfsadf

--- Validation.php
+++ Validation.php
@@ -1,5 +1,14 @@
 <?php
 
 class Validation{
-	
+	/**
+	 * @author	Budi Pratama <budi.pratama@icode.co.id>
+	 * @copyright	PT. Indocode Technology (https://www.icode.co.id/)
+	 * @since	Version 1.0.0
+	 * @filesource
+	 */
+	public function ()
+	{
+		
+	}
 }
